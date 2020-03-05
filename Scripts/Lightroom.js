df36b5a2a695c2508fcc3e66a70483cc0c84e8f3
Peng-YM <1048217874@qq.com>
2020-03-05 18:52:29 +0800

    Add scripts
    Adobe Lightroom

---
+++ Scripts/Lightroom.js
@@ -0,0 +1,34 @@
+body = $response.body.replace(/while.{7}\n/, "");
+let obj = JSON.parse(body);
+obj.entitlement.status="subscriber";
+obj.current_subs={
+      "product_id": "lightroom",
+      "store": "adobe",
+      "purchase_date": "2019-10-10T16:32:10.254954Z",
+      "sao": {
+        "inpkg_CCES": "0",
+        "inpkg_CCLE": "1",
+        "inpkg_CCSN": "0",
+        "inpkg_CCSV": "0",
+        "inpkg_LCCC": "0",
+        "inpkg_LPES": "0",
+        "inpkg_LRBRL": "0",
+        "inpkg_LRMAC": "0",
+        "inpkg_LRMC": "0",
+        "inpkg_LRMP": "0",
+        "inpkg_LRTB": "0",
+        "inpkg_PHLT": "0",
+        "inpkg_PHLT2": "0",
+        "inpkg_PLES": "0",
+        "storage_quota": "100"
+      }
+    };
+obj.entitlement.storage={
+      "used": 0,
+      "limit": 1154487209165,
+      "display_limit": 1099511627776,
+      "warn": 992137445376
+    };
+obj.avatar.placeholder=true;
+body = JSON.stringify(obj); 
+$done({body});
