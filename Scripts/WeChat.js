c7204420d6b5d33f8150c5140050038e9176a29e
Peng-YM <1048217874@qq.com>
2020-05-04 22:59:49 +0800

    Update WeChat adblock

--- Scripts/WeChat.js
+++ Scripts/WeChat.js
@@ -1,7 +1,5 @@
-var data = {
-  body: "{}",
-  headers: {
-    "Content-Type": "application/json"
-  }
-};
-$done({response: data});
+var obj = JSON.parse($response.body);
+obj.advertisement_num = 0;
+obj.advertisement_info = [];
+delete obj.appid;
+$done({body: JSON.stringify(obj)});
