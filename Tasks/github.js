5f1972544b4fa623377fe59a83a2e1d90b60100a
Peng-YM <1048217874pengym@gmail.com>
2020-06-12 14:14:41 +0800

    Update github.js

--- Tasks/github.js
+++ Tasks/github.js
@@ -4,6 +4,7 @@
  * 更新地址：https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/github.js
  * 配置方法：
  * 1. 填写github token, 在github > settings > developer settings > personal access token 里面生成一个新token。
+ * 默认TOKEN用的是我自己的，请不要请求过于频繁，每天一两次即可。例如：cron "0 9 * * *"
  * 2. 配置仓库地址，格式如下：
  * {
  *  name: "仓库名称",
