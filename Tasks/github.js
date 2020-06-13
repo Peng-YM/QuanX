88b533c61f92ad14fb6788d8641146c12cc536cc
toulanboy <toulanboy@qq.com>
2020-06-13 14:54:49 +0800

    新增具体文件监控

--- Tasks/github.js
+++ Tasks/github.js
@@ -3,7 +3,7 @@
  * 1、监控github仓库的commits和release。
  * 2、监控具体的文件或目录是否有更新。
  * @author: Peng-YM， toulanboy
- * 更新地址：https://github.com/toulanboy/github_detect
+ * 更新地址：https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/github.js
  * 配置方法：
  * 1. 填写github token, 在github > settings > developer settings > personal access token 里面生成一个新token。
  * 默认TOKEN用的是我自己的，请不要请求过于频繁，每天一两次即可。例如：cron "0 9 * * *"* 2. 配置仓库地址，格式如下：
