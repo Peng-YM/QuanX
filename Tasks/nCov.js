de7c206c6ea32c2b8d5040a6f31914846fcb9e4e
Peng-YM <1048217874@qq.com>
2020-06-18 16:57:19 +0800

    Updated nCov

--- Tasks/nCov.js
+++ Tasks/nCov.js
@@ -39,7 +39,7 @@ const headers = {
   let subtitle = `🗓 ${formatTime()}`;
   let detail =
     "「数据统计」" +
-    "\n\n    -新增确诊: " +
+    "\n    -新增确诊: " +
     overall.currentConfirmedIncr +
     "\n    -现有确诊: " +
     overall.currentConfirmedCount +
@@ -49,9 +49,9 @@ const headers = {
     overall.curedCount +
     "\n    -死亡: " +
     overall.deadCount +
-    "\n\n「疫情动态」\n\n     " +
+    "\n「疫情动态」\n\n     " +
     news.title +
-    "\n\n「动态详情」\n\n     " +
+    "\n「动态详情」\n\n     " +
     news.summary;
   $.notify(title, subtitle, detail);
 })()
