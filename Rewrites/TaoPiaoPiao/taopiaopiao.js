c725f95eee28cdc5fcb489204a2c6afa228955b8
Peng-YM <1048217874pengym@gmail.com>
2021-05-11 15:46:23 +0800

    修正某些电影没有修改的bug

--- Rewrites/TaoPiaoPiao/taopiaopiao.js
+++ Rewrites/TaoPiaoPiao/taopiaopiao.js
@@ -38,7 +38,7 @@ const imdbId = body.data.returnValue.imdbid;
 function queryDoubanInfo({ title, imdbId }) {
     $.info(`Querying Douban info for movie: ${title}`);
     return $.http.get({
-        url: `https://www.douban.com/search?cat=1002&q=${encodeURIComponent(imdbId)}`,
+        url: `https://www.douban.com/search?cat=1002&q=${encodeURIComponent(imdbId || title)}`,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15',
             'Cookie': JSON.stringify(Date.now())
